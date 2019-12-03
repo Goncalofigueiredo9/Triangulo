515447b266deda9095eec13d55f11b2df53b201f
GoncaloF <GoncaloF@WIN-GESFLHLR4EE>
2019-12-03 16:45:43 +0000

    Adicionar arquivos de projeto.

---
+++ Triangulo/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace Triangulo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.x1 = new System.Windows.Forms.TextBox();
+            this.x2 = new System.Windows.Forms.TextBox();
+            this.x3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.y1 = new System.Windows.Forms.TextBox();
+            this.y2 = new System.Windows.Forms.TextBox();
+            this.y3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.z3 = new System.Windows.Forms.TextBox();
+            this.z2 = new System.Windows.Forms.TextBox();
+            this.z1 = new System.Windows.Forms.TextBox();
+            this.area = new System.Windows.Forms.RadioButton();
+            this.perimetro = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(423, 378);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // x1
+            // 
+            this.x1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.x1.Location = new System.Drawing.Point(317, 150);
+            this.x1.Name = "x1";
+            this.x1.Size = new System.Drawing.Size(100, 31);
+            this.x1.TabIndex = 1;
+            // 
+            // x2
+            // 
+            this.x2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.x2.Location = new System.Drawing.Point(317, 187);
+            this.x2.Name = "x2";
+            this.x2.Size = new System.Drawing.Size(100, 31);
+            this.x2.TabIndex = 2;
+            // 
+            // x3
+            // 
+            this.x3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.x3.Location = new System.Drawing.Point(317, 224);
+            this.x3.Name = "x3";
+            this.x3.Size = new System.Drawing.Size(100, 31);
+            this.x3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(213, 317);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(471, 58);
+            this.label1.TabIndex = 4;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(235, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ponto 1";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(235, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 27);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ponto 2";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(235, 228);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Ponto 3";
+            // 
+            // y1
+            // 
+            this.y1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.y1.Location = new System.Drawing.Point(423, 150);
+            this.y1.Name = "y1";
+            this.y1.Size = new System.Drawing.Size(100, 31);
+            this.y1.TabIndex = 8;
+            // 
+            // y2
+            // 
+            this.y2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.y2.Location = new System.Drawing.Point(423, 187);
+            this.y2.Name = "y2";
+            this.y2.Size = new System.Drawing.Size(100, 31);
+            this.y2.TabIndex = 9;
+            // 
+            // y3
+            // 
+            this.y3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.y3.Location = new System.Drawing.Point(423, 224);
+            this.y3.Name = "y3";
+            this.y3.Size = new System.Drawing.Size(100, 31);
+            this.y3.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(358, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "x";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(463, 112);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "y";
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(234, 45);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(338, 33);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Perímetro de um Triângulo";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(573, 112);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(17, 20);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "z";
+            // 
+            // z3
+            // 
+            this.z3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.z3.Location = new System.Drawing.Point(529, 224);
+            this.z3.Name = "z3";
+            this.z3.Size = new System.Drawing.Size(100, 31);
+            this.z3.TabIndex = 17;
+            // 
+            // z2
+            // 
+            this.z2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.z2.Location = new System.Drawing.Point(529, 187);
+            this.z2.Name = "z2";
+            this.z2.Size = new System.Drawing.Size(100, 31);
+            this.z2.TabIndex = 16;
+            // 
+            // z1
+            // 
+            this.z1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.z1.Location = new System.Drawing.Point(529, 150);
+            this.z1.Name = "z1";
+            this.z1.Size = new System.Drawing.Size(100, 31);
+            this.z1.TabIndex = 15;
+            // 
+            // area
+            // 
+            this.area.AutoSize = true;
+            this.area.Location = new System.Drawing.Point(362, 280);
+            this.area.Name = "area";
+            this.area.Size = new System.Drawing.Size(47, 17);
+            this.area.TabIndex = 18;
+            this.area.TabStop = true;
+            this.area.Text = "Area";
+            this.area.UseVisualStyleBackColor = true;
+            // 
+            // perimetro
+            // 
+            this.perimetro.AutoSize = true;
+            this.perimetro.Location = new System.Drawing.Point(488, 280);
+            this.perimetro.Name = "perimetro";
+            this.perimetro.Size = new System.Drawing.Size(69, 17);
+            this.perimetro.TabIndex = 19;
+            this.perimetro.TabStop = true;
+            this.perimetro.Text = "Perimetro";
+            this.perimetro.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.perimetro);
+            this.Controls.Add(this.area);
+            this.Controls.Add(this.z3);
+            this.Controls.Add(this.z2);
+            this.Controls.Add(this.z1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.y3);
+            this.Controls.Add(this.y2);
+            this.Controls.Add(this.y1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.x3);
+            this.Controls.Add(this.x2);
+            this.Controls.Add(this.x1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox x1;
+        private System.Windows.Forms.TextBox x2;
+        private System.Windows.Forms.TextBox x3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox y1;
+        private System.Windows.Forms.TextBox y2;
+        private System.Windows.Forms.TextBox y3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox z3;
+        private System.Windows.Forms.TextBox z2;
+        private System.Windows.Forms.TextBox z1;
+        private System.Windows.Forms.RadioButton area;
+        private System.Windows.Forms.RadioButton perimetro;
+    }
+}
+
